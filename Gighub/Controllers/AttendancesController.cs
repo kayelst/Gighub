e486870cc6aeeaf000e81cc8c8a00fedb383e58d
Kay Elst <kayelst@gmail.com>
2018-09-07 14:51:32 +0200

    Added going button with API call

---
+++ Gighub/Controllers/AttendancesController.cs
@@ -0,0 +1,46 @@
+﻿using Gighub.Dto_s;
+using Gighub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Gighub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+            {
+                return BadRequest("The Attendance already exists");
+            }
+
+            var attendance = new Attendance
+                {
+                 GigId = dto.GigId,
+                 AttendeeId = userId
+            };
+
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
