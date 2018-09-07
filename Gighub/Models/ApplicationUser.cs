beba831cdaaf291335631416a68e6402bd667e1a
Kay Elst <kayelst@gmail.com>
2018-09-07 08:23:23 +0200

    Setting Up Database

--- Gighub/Models/ApplicationUser.cs
+++ Gighub/Models/ApplicationUser.cs
@@ -15,19 +15,10 @@ namespace Gighub.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+
         }
+      
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
 }
